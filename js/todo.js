9f0f372ffa71801970c992d97bd3418d7c571084
LS-C <suzy.cho111@gmail.com>
2017-09-22 21:35:56 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,25 @@
+
+
+
+
 $(document).ready(function(){
+
+submitForm();
   // call functions here
 });
 
 // define functions here
+
+
+
+function submitForm(){
+  $('form').on('submit', function(event){
+    var new_task = $('#item').val();
+    $('ol').append( '<li>' + new_task + '</li>' );
+    event.preventDefault();
+  });
+}
+
+
+
+
